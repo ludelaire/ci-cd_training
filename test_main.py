ee6199f95f8a142bbe1f9f1274c59c5e722f02b0
ludelaire <luisaalarcons30@gmail.com>
2022-02-09 11:04:04 -0500

    Updated requirements, erased class in test.

--- test_main.py
+++ test_main.py
@@ -1,11 +1,9 @@
-import unittest
 from main import suma
 
 
-class TestSuma(unittest.TestCase):
-    def test_positive_negative_result_zero(self):
-        a = 9
-        b = -9
-        c = suma(a, b)
+def test_positive_negative_result_zero():
+    a = 9
+    b = -9
+    c = suma(a, b)
 
-        self.assertTrue(c == 0)
+    assert c == 0
